9cebb3c805ccf3c498ef690c49e60c2a865c3025
akash <akash.ry@somaiya.edu>
2021-07-21 23:11:40 +0530

    Made Few Improvements

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const nav = document.querySelector('nav')
 const tabs = document.querySelectorAll('[data-target-tab]')
 const tabContents = document.querySelectorAll('.content')
 
-document.addEventListener('scroll',scrollAppear)
+document.addEventListener('scroll', scrollAppear)
 scrollAppear()
-function scrollAppear(){
+function scrollAppear() {
     var skillsUL = document.querySelector('.skill-container ul');
     const skillContent = document.querySelector('.skill-container .tab-content')
     var skillPosition = skillsUL.getBoundingClientRect().top;
@@ -18,18 +18,18 @@ function scrollAppear(){
 
     const education = document.querySelector('#education')
     const educationPosition = education.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight/1.2
+    var screenPosition = window.innerHeight / 1.2
     // console.log(skillPosition,screenPosition)
 
-    if (skillPosition < screenPosition ){
+    if (skillPosition < screenPosition) {
         skillsUL.classList.add('animated')
         skillContent.classList.add('animated')
     }
-    if(cardPosition < screenPosition){
+    if (cardPosition < screenPosition) {
         cardContainer.classList.add('animated')
     }
 
-    if (educationPosition < screenPosition){
+    if (educationPosition < screenPosition) {
         education.classList.add('animated')
     }
 
@@ -38,14 +38,14 @@ function scrollAppear(){
 
 
 
-tabs.forEach(tab=>{
-    tab.addEventListener('click', ()=>{
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.targetTab)
-        tabContents.forEach(tabContent=>{
+        tabContents.forEach(tabContent => {
             tabContent.classList.remove('active')
         })
 
-        tabs.forEach(tab =>{
+        tabs.forEach(tab => {
             tab.classList.remove('active')
         })
         tab.classList.add('active')
@@ -53,13 +53,13 @@ tabs.forEach(tab=>{
     })
 })
 
-links.forEach((link)=>{
-    link.addEventListener('click',()=>{
-        links.forEach(nlink =>{
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        links.forEach(nlink => {
             nlink.parentElement.classList.remove('active')
         })
         link.parentElement.classList.add('active')
-        if(nav.classList.contains('nav-active')){
+        if (nav.classList.contains('nav-active')) {
             nav.classList.remove('nav-active')
             burger.classList.remove('toggle')
         }
@@ -70,48 +70,48 @@ document.addEventListener("mousemove", parallax);
 
 
 const checkbox = document.querySelector('#checkbox')
-checkbox.addEventListener('change',()=>{
+checkbox.addEventListener('change', () => {
     document.body.classList.toggle('dark')
 })
 
 const img = document.querySelector('#akash')
-img.addEventListener('mouseover', ()=>{
+img.addEventListener('mouseover', () => {
     img.src = "./images/wink.jpeg"
-}) 
-img.addEventListener('mouseout',()=>{
+})
+img.addEventListener('mouseout', () => {
     img.src = "./images/smile.jpeg"
 })
 
 
 
-burger.addEventListener('click',()=>{
+burger.addEventListener('click', () => {
     burger.classList.toggle('toggle')
     nav.classList.toggle('nav-active')
-    links.forEach((link,index)=>{
-        if(link.style.animation){
+    links.forEach((link, index) => {
+        if (link.style.animation) {
             link.style.animation = ''
         }
-        else{
-            link.style.animation = `navLinkFade 0.2s forwards ${index/5 + 0.2}s`
+        else {
+            link.style.animation = `navLinkFade 0.2s forwards ${index / 5 + 0.2}s`
         }
-        console.log(index/7)
+        console.log(index / 7)
     })
 
 })
 
-function parallax(e){
-    document.querySelectorAll('.layer').forEach(layer=>{
+function parallax(e) {
+    document.querySelectorAll('.layer').forEach(layer => {
         const speed = layer.getAttribute('data-speed')
 
-        const x = (window.innerWidth - e.pageX*speed)/100
-        const y = (window.innerHeight - e.pageY*speed)/100
+        const x = (window.innerWidth - e.pageX * speed) / 100
+        const y = (window.innerHeight - e.pageY * speed) / 100
 
-        layer.style.opacity=`0.7`
+        layer.style.opacity = `0.7`
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
     })
 }
 
-console.log("%cBuilt With pure CSS AND vanilla Javascript By Akash Yadav ❤","font-weight:bold; font-size:3rem; color:#f1c40f;")
+console.log("%cBuilt With pure CSS AND vanilla Javascript By Akash Yadav ❤", "font-weight:bold; font-size:3rem; color:#f1c40f;")
 
 
 const copyYear = document.querySelector('.copy-year')
