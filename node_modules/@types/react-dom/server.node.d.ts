526ad3d769233be392ea8f2f28f3e1281157cbc6
akash-yadav12 <akash.ry@somaiya.edu>
2026-09-13 23:13:08 +0530

    chore: add .gitignore and untrack node_modules and tsbuildinfo

--- node_modules/@types/react-dom/server.node.d.ts
+++
@@ -1,9 +0,0 @@
-export {
-    renderToPipeableStream,
-    renderToReadableStream,
-    renderToStaticMarkup,
-    renderToString,
-    resume,
-    resumeToPipeableStream,
-    version,
-} from "./server";
